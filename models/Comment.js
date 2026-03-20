5e842b4e00c7cf8cc8d263ea0384db202c0cd145
Chris <106016563+Xtosea@users.noreply.github.com>
2026-03-20 05:06:02 +0100

    Update Comment.js

--- models/Comment.js
+++ models/Comment.js
@@ -4,11 +4,11 @@ const commentSchema = new mongoose.Schema(
   {
     post: { type: mongoose.Schema.Types.ObjectId, ref: "Post", required: true },
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    content: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now }
+    text: { type: String, required: true },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   },
   { timestamps: true }
 );
 
-// Export default so your import works
-export default mongoose.model("Comment", commentSchema);
+const Comment = mongoose.model("Comment", commentSchema);
+export default Comment;
