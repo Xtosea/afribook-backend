6b0c0584baf518ab5632931de72dabb5e297cb44
Chris <106016563+Xtosea@users.noreply.github.com>
2026-03-25 14:33:48 +0100

    Update searchRoutes.js

--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -6,21 +6,19 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
   try {
-    const q = req.query.q || "";
+    const search = req.query.q;
 
-    const users = await User.find({
-      name: { $regex: q, $options: "i" }
-    }).select("name profilePic");
+const users = await User.find({
+name: { $regex: search, $options: "i" }
+}).select("name profilePic");
 
-    const posts = await Post.find({
-      content: { $regex: q, $options: "i" }
-    }).populate("user", "name profilePic");
+res.json(users);
 
-    res.json({ users, posts });
-  } catch (err) {
-    console.error("SEARCH ERROR:", err);
-    res.status(500).json({ error: "Server error" });
-  }
+} catch (err) {
+
+res.status(500).json({ error: "Server error" });
+
+}
 });
 
 export default router;
