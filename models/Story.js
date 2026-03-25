48fd0dfade30e2a982431a0fee8518fb4bad8be4
Chris <106016563+Xtosea@users.noreply.github.com>
2026-03-25 17:18:54 +0100

    Update Story.js

--- models/Story.js
+++ models/Story.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
 
 const storySchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  media: String,
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  media: { type: String, required: true }, // URL to image or video
   type: { type: String, enum: ["image", "video"], default: "image" },
   views: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  reactions: [{ user: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, type: String }],
+  reactions: [{ 
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, 
+    type: { type: String } 
+  }],
   createdAt: { type: Date, default: Date.now, expires: 86400 } // auto delete after 24h
 });
 
