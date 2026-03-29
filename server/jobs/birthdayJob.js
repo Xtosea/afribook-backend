923b800a3f49eb2f16dcee68a0ce90eaa7e0bcd1
Chris <106016563+Xtosea@users.noreply.github.com>
2026-03-29 22:28:34 +0100

    Update birthdayJob.js

--- server/jobs/birthdayJob.js
+++ server/jobs/birthdayJob.js
@@ -1,33 +1,43 @@
 import User from "../models/User.js";
 import Post from "../models/Post.js";
+import { getSocket } from "../socket.js";
 
 export const checkBirthdays = async () => {
   try {
     const today = new Date();
-    const month = today.getMonth() + 1;
+    const month = today.getMonth() + 1; // JS months are 0-indexed
     const day = today.getDate();
 
+    // Fetch only users whose birthday is today
     const users = await User.find({
-      dob: { $exists: true, $ne: null }
+      dob: { $exists: true, $ne: null },
+      $expr: {
+        $and: [
+          { $eq: [{ $dayOfMonth: "$dob" }, day] },
+          { $eq: [{ $month: "$dob" }, month] }
+        ]
+      }
     });
 
-    for (let user of users) {
-      const dob = new Date(user.dob);
+    const io = getSocket();
 
-      if (
-        dob.getDate() === day &&
-        dob.getMonth() + 1 === month
-      ) {
-        await Post.create({
-          user: user._id,
-          text: `🎉 Today is ${user.name}'s birthday! Wish them well.`,
-          type: "birthday"
-        });
-      }
+    for (let user of users) {
+      // Create birthday post
+      await Post.create({
+        user: user._id,
+        text: `🎉 Today is ${user.name}'s birthday! Wish them well.`,
+        type: "birthday"
+      });
+
+      // Emit socket event
+      io.emit("birthday", {
+        userId: user._id,
+        name: user.name
+      });
     }
 
-    console.log("Birthday check complete");
+    console.log(`Birthday check complete. ${users.length} birthday(s) today.`);
   } catch (err) {
-    console.error(err);
+    console.error("Error checking birthdays:", err);
   }
 };
