f4a7f7684c2c49d8882f7450405c0057f9eadd18
Chris <106016563+Xtosea@users.noreply.github.com>
2026-05-19 22:22:34 +0100

    Update aiRoutes.js

--- routes/aiRoutes.js
+++ routes/aiRoutes.js
@@ -24,4 +24,47 @@ router.post("/enhance", async (req, res) => {
   }
 });
 
+
+router.post(
+  "/register",
+  async (req, res) => {
+
+    const {
+      name,
+      email,
+      password,
+      referralCode,
+    } = req.body;
+
+    // CREATE USER
+    const user =
+      await User.create({
+        name,
+        email,
+        password,
+      });
+
+    // IF REFERRED
+    if (referralCode) {
+
+      const referrer =
+        await User.findOne({
+          referralCode,
+        });
+
+      if (referrer) {
+
+        await addPoints(
+          referrer._id,
+          500,
+          "referral"
+        );
+      }
+    }
+
+    res.json(user);
+  }
+);
+
+
 export default router;
