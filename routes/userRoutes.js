e3654adc2be1045b231cd49c53c828eeede0f3a7
Chris <106016563+Xtosea@users.noreply.github.com>
2026-05-20 09:16:43 +0100

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -313,4 +313,38 @@ router.get("/:userId", async (req, res) => {
 });
 
 
+// GET SINGLE USER
+router.get(
+  "/:id",
+  verifyToken,
+  async (req, res) => {
+
+    try {
+
+      const user =
+        await User.findById(
+          req.params.id
+        ).select("-password");
+
+      if (!user) {
+        return res
+          .status(404)
+          .json({
+            message:
+              "User not found",
+          });
+      }
+
+      res.json(user);
+
+    } catch (err) {
+
+      res.status(500).json({
+        message:
+          "Server error",
+      });
+    }
+  }
+);
+
 export default router;
