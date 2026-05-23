8a775ab1bb4403ced7a8d6101230cf687eb84781
Chris <106016563+Xtosea@users.noreply.github.com>
2026-05-23 16:14:18 +0100

    Update contactRoutes.js

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { authMiddleware } from "../middleware/authMiddleware.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 import { syncContacts } from "../controllers/contactController.js";
 
 const router = express.Router();
