d05f6e23f44a2fc4b6e3fcb1cd672b87d2ba222d
Chris <106016563+Xtosea@users.noreply.github.com>
2026-05-23 16:11:18 +0100

    Update contactRoutes.js

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,10 +1,9 @@
 import express from "express";
-import authMiddleware from "../middleware/authMiddleware.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 import { syncContacts } from "../controllers/contactController.js";
 
 const router = express.Router();
 
-// 👇 THIS is where your line goes
 router.post("/sync", authMiddleware, syncContacts);
 
 export default router;
