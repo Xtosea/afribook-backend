f38295bb096b3981d25c62159389bf64f6b6623e
Chris <106016563+Xtosea@users.noreply.github.com>
2026-05-31 21:28:08 +0100

    Update r2StoryRoutes.js

--- routes/r2StoryRoutes.js
+++ routes/r2StoryRoutes.js
@@ -2,6 +2,8 @@ import express from "express";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { verifyToken } from "../middleware/authMiddleware.js";
+import { sendNotification }
+from "../utils/sendNotification.js";
 
 const router = express.Router();
 
