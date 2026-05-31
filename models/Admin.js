431d8890b04195ee243def5038ec9076aff6c7c8
Chris <106016563+Xtosea@users.noreply.github.com>
2026-05-31 22:55:25 +0100

    Create Admin.js

---
+++ models/Admin.js
@@ -0,0 +1,29 @@
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: String,
+    password: String,
+
+    profilePic: String,
+
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+
+    verificationStatus: {
+      type: String,
+      default: "PENDING",
+    },
+
+    // 👇 ADD THIS
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
