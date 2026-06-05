32f2dc328515e00fcdf4a9a287fa7608c5ac9853
Chris <106016563+Xtosea@users.noreply.github.com>
2026-06-05 16:41:39 +0100

    Update AdminRoutes.js

--- routes/AdminRoutes.js
+++ routes/AdminRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { verifyToken } from "../middleware/authMiddleware.js";
+
 import { sendNotification } from "../utils/sendNotification.js";
 
 import Verification from "../models/Verification.js";
