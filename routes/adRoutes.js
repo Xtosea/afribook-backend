a08bf94f15dc4e7eae3cf63397fb23941371c574
Chris <106016563+Xtosea@users.noreply.github.com>
2026-06-05 11:05:34 +0100

    Update adRoutes.js

--- routes/adRoutes.js
+++ routes/adRoutes.js
@@ -1,1 +1,47 @@
+import express from "express";
 
+import {
+  createCampaign,
+  getMyCampaigns,
+  serveAd,
+  recordImpression,
+  recordClick,
+} from "../controllers/adController.js";
+
+import {
+  verifyToken,
+} from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.post(
+  "/create",
+  verifyToken,
+  createCampaign
+);
+
+router.get(
+  "/my-campaigns",
+  verifyToken,
+  getMyCampaigns
+);
+
+router.get(
+  "/serve",
+  verifyToken,
+  serveAd
+);
+
+router.post(
+  "/impression",
+  verifyToken,
+  recordImpression
+);
+
+router.post(
+  "/click",
+  verifyToken,
+  recordClick
+);
+
+export default router;
