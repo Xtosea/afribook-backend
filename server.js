17bc00862f0fd0f5a730851531ec78cf646de8b8
Chris <106016563+Xtosea@users.noreply.github.com>
2026-06-14 13:34:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ from "./routes/adminRoutes.js";
 import earningRoutes
 from "./routes/earningRoutes.js";
 import adminStatsRoutes from "./routes/adminStatsRoutes.js";
-import storyCloudnaryRoutes from "./routes/storyCloudnaryRoutes";
+import storyCloudnaryRoutes from "./routes/storyCloudnaryRoutes.js";
 
 
 
