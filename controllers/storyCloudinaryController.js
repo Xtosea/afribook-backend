6a6066045d103af0f30a5e366ee2b43c1be608e3
Chris <106016563+Xtosea@users.noreply.github.com>
2026-06-17 10:18:18 +0100

    Update storyCloudinaryController.js

--- controllers/storyCloudinaryController.js
+++ controllers/storyCloudinaryController.js
@@ -3,6 +3,7 @@ import cloudinary from "../config/cloudinary.js";
 import fs from "fs";
 
 
+
 export const uploadPostImage = async (req, res) => {
   try {
     console.log("FILE:", req.file);
@@ -16,22 +17,24 @@ export const uploadPostImage = async (req, res) => {
     const result = await cloudinary.uploader.upload(
       req.file.path,
       {
-        folder: "afribook/posts",
-        resource_type: "image",
+        folder: "afribook/stories",
+        resource_type: "auto",
       }
     );
 
     fs.unlinkSync(req.file.path);
 
-    res.json({
+    return res.json({
       url: result.secure_url,
     });
 
   } catch (err) {
-    console.error("CLOUDINARY ERROR:", err);
+    console.error("CLOUDINARY ERROR:");
+    console.error(err);
 
-    res.status(500).json({
+    return res.status(500).json({
       error: err.message,
+      cloudinaryError: err,
     });
   }
 };
