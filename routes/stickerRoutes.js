42d15b06e3d50962ce75b7c791eb7c75059ca2a0
Chris <106016563+Xtosea@users.noreply.github.com>
2026-07-19 13:51:19 +0100

    Update stickerRoutes.js

--- routes/stickerRoutes.js
+++ routes/stickerRoutes.js
@@ -1,7 +1,27 @@
 import express from "express";
-import stickers from "../data/stickers.js";
 
 const router = express.Router();
+
+const stickers = [
+  {
+    _id: "1",
+    name: "Smile",
+    url: "/stickers/smile.webp",
+    category: "emoji",
+  },
+  {
+    _id: "2",
+    name: "Love",
+    url: "/stickers/love.webp",
+    category: "love",
+  },
+  {
+    _id: "3",
+    name: "Fire",
+    url: "/stickers/fire.webp",
+    category: "emoji",
+  },
+];
 
 router.get("/", (req, res) => {
   res.json(stickers);
