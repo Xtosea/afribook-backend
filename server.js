36667eaaf4fc2b657a3690a106be1c704e8b003e
Chris <106016563+Xtosea@users.noreply.github.com>
2026-07-29 14:05:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -152,6 +152,13 @@ app.use((req, res, next) => {
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
+//DEBUGGING ONLY 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.originalUrl}`);
+  next();
+});
+
+
 /* ================= FILEUPLOAD ================= */
 app.use(
   fileUpload({
